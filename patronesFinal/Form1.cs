6a9074aa5ad83dc5d2e2cac62c9eb81b97aaae6a
AlexBamaca <71303844+AlexBamaca@users.noreply.github.com>
2020-12-14 22:17:16 -0800

    diseños de login

--- patronesFinal/Form1.cs
+++ patronesFinal/Form1.cs
@@ -16,5 +16,10 @@ namespace patronesFinal
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
