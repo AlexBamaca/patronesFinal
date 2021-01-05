59f1c2ffe9da0971b8d14b1acac067f4f74ffbc1
AlexBamaca <71303844+AlexBamaca@users.noreply.github.com>
2021-01-04 20:20:51 -0800

    Envio de var. 8:20

--- patronesFinal/Form1.cs
+++ patronesFinal/Form1.cs
@@ -66,7 +66,8 @@ namespace patronesFinal
             {
                 string nombre2;
                 nombre2 = "ESDRAS";
-                MessageBox.Show("Bienvenido de nuevo.");
+
+                MessageBox.Show();
                 this.Hide();
                 Form3 F = new Form3(nombre2);
                 F.Show();
